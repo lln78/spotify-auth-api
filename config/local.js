bd526781d4fd904114225093161758643a4c0a85
lln78 <llauden@ijinus.fr>
2022-01-21 16:38:42 +0100

    UPD : scopes

--- config/local.js
+++ config/local.js
@@ -10,5 +10,5 @@ export default {
   clientSecret: '755c207f6cd24a56b3d9bd7a10cf28fc',
   redirectUri: 'http://localhost:4040/callback',
 
-  scope: 'user-read-private '
+  scope: 'user-read-private ugc-image-upload user-read-playback-state user-modify-playback-state user-follow-modify user-follow-read user-library-modify user-library-read streaming user-read-playback-position playlist-modify-private playlist-read-collaborative app-remote-control user-read-email playlist-read-private user-top-read playlist-modify-public user-read-currently-playing user-read-recently-played '
 }
--- config/prod.js
+++ config/prod.js
@@ -10,5 +10,5 @@ export default {
   clientSecret: '4e2c13a72f4f4e44b359cd03b564d666',
   redirectUri: 'https://spotify-lln-auth.herokuapp.com/callback',
 
-  scope: 'user-read-private'
+  scope: 'user-read-private ugc-image-upload user-read-playback-state user-modify-playback-state user-follow-modify user-follow-read user-library-modify user-library-read streaming user-read-playback-position playlist-modify-private playlist-read-collaborative app-remote-control user-read-email playlist-read-private user-top-read playlist-modify-public user-read-currently-playing user-read-recently-played '
 }
