e7bd01bec991408754aa03884c9b1859b3e56585
lln78 <llauden@laposte.net>
2022-01-21 10:15:13 +0100

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,14 +1,14 @@
 export default {
   port: 4041,
 
-  clientURL: 'https://gk4m.github.io/vue-spotify/',
+  clientURL: 'https://spotify-lln.herokuapp.com/',
 
   spotifyAPI: 'https://api.spotify.com/v1/',
   spotifyAccountsAPI: 'https://accounts.spotify.com/',
 
-  clientID: '6eb45d91629c4f6c803a67f97a7f8fb1',
-  clientSecret: '755c207f6cd24a56b3d9bd7a10cf28fc',
-  redirectUri: 'https://stark-harbor-25810.herokuapp.com/callback',
+  clientID: '464493aca63942e0acc8d9938ee3a184',
+  clientSecret: '4e2c13a72f4f4e44b359cd03b564d666',
+  redirectUri: 'https://spotify-auth-lln.herokuapp.com/callback',
 
   scope: 'user-read-private user-read-email user-read-birthdate user-read-playback-state user-modify-playback-state user-library-read playlist-read-private streaming playlist-modify-public playlist-modify-private user-library-modify ugc-image-upload user-follow-modify user-follow-read'
 }
