375080b38288e44539b426de3cd6907a50cb0de3
lln78 <llauden@laposte.net>
2022-01-21 16:04:19 +0100

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -8,7 +8,7 @@ export default {
 
   clientID: '464493aca63942e0acc8d9938ee3a184',
   clientSecret: '4e2c13a72f4f4e44b359cd03b564d666',
-  redirectUri: 'https://spotify-auth-lln.herokuapp.com/callback',
+  redirectUri: 'https://spotify-lln-auth.herokuapp.com/callback',
 
   scope: 'user-read-private user-read-email user-read-birthdate user-read-playback-state user-modify-playback-state user-library-read playlist-read-private streaming playlist-modify-public playlist-modify-private user-library-modify ugc-image-upload user-follow-modify user-follow-read'
 }
